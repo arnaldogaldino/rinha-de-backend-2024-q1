dc86f6578ba6cc0c8fe8bc4031359132c017cd20
arnaldogaldino <arnaldo.galdino@gmail.com>
2024-02-07 16:50:03 -0300

    criando projeto

---
+++ participantes/arnaldogaldino/RinhaBackend/RinhaBackend/Models/Transacao.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RinhaBackend.Models
+{
+    public class Transacao
+    {
+        [Required(ErrorMessage = "Campo valor é obrigatório")]
+        public int Valor { get; set; }
+
+        [Required(ErrorMessage = "Campo tipo é obrigatório")]
+        [StringLength(1)]
+        [AllowedValues("c", "d")]
+        public string Tipo { get; set; }
+
+        [Required(ErrorMessage = "Campo descrição é obrigatório")]
+        [Length(1, 10)]
+        public string Descricao { get; set; }
+    }
+}
